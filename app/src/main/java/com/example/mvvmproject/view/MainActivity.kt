cae692ec1b1448c584c26d97a83568a84c8f81b0
Diana Mocoh <mocohdiana04@gmail.com>
2022-09-09 12:11:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mvvmproject/view/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mvvmproject.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.Observer
+import com.catalin.mvvm_cities.R
+import com.catalin.mvvm_cities.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
